2aec634ade8be7db6b10542e83adf5e8e8c2de34
K4liber <janbielecki94@gmail.com>
2021-08-24 20:08:03 +0200

    docker + nginx

--- flask/main.py
+++ flask/main.py
@@ -43,4 +43,4 @@ def location():
     return str(data.location)
 
 
-app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0')
--- flask/static/load_map.js
+++ flask/static/load_map.js
@@ -1,4 +1,4 @@
-function load_map(lat, lon) {
+function load_map(lat, lon, last_timestamp) {
     var mymap = L.map('mapid').setView([lat, lon], 13);
 
     var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiazRsaWJlciIsImEiOiJja3NtczE4MmUwMW9jMnBucDZkdWYyZ2JzIn0.bRAZ1jLsbV1tY1-zNr9UzA', {
@@ -14,7 +14,8 @@ function load_map(lat, lon) {
     var marker = L.marker([lat, lon]).addTo(mymap);
     marker.bindPopup("" +
         "<div style='margin: 0 auto;'>" +
-        "<img style='width: 80px;' src='static/jbielecki.jpeg'/>" +
+        "<img style='width: 80px;' src='static/images/jbielecki.jpeg'/>" +
         "<br>Jan Bielecki" +
+        "<br>" + last_timestamp +
         "</div>");
 }
